c7737a51e49304cc14bcb634839187f58e433eae
Alex Overbeck <alexoverbeck@gmail.com>
2019-01-31 16:31:27 -0500

    Remove default telegram text

--- telegram.rb
+++ telegram.rb
@@ -1,8 +1,5 @@
 class Telegram
   def self.build(line_length:, text:)
-    line_length ||= 50
-    text ||= "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
     truncate_lines(max_line_length: line_length, text: text)
   end
 
