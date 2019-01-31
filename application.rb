5f9665ca8cae6d5c60df447ed77661b111c53c0b
Alex Overbeck <alexoverbeck@gmail.com>
2019-01-31 16:06:29 -0500

    Add endpoint logging

--- application.rb
+++ application.rb
@@ -1,17 +1,29 @@
 require 'sinatra'
-require 'sinatra/reloader' if development?
-require 'pry' if development?
+require 'logger'
 require 'json'
 require './telegram'
 
+if development?
+  require 'sinatra/reloader' 
+  require 'pry'
+end
+
+set :logger, Logger.new(STDOUT)
+
+# Basic telegram test
 get '/telegram' do
-  Telegram.build(line_length: params[:w], text: params[:text]).join("<br>")
+  telegram = Telegram.build(line_length: params[:w], text: params[:text])
+  
+  logger.info "\n/telegram used -- Telegram: \n\n #{telegram.join("\n")} \n\n==================================================="
+  
+  telegram.join('<br>')
 end
 
 post '/telegram.json' do
   content_type :json
   telegram = Telegram.build(line_length: params[:w], text: params[:text])
 
+  logger.info "\n/telegram.json -- Telegram: \n\n #{telegram.join("\n")} \n\n==================================================="
   {
     telegram_lines: telegram,
     line_count: telegram.length
